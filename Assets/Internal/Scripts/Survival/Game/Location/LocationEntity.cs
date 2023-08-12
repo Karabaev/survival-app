4ed12b27c634f0147cb4c2601c309b7957c11ba1
Karabin <maxkarab@yandex.ru>
2023-08-12 19:59:24 +0600

    Refactored location

--- Assets/Internal/Scripts/Survival/Game/Location/LocationEntity.cs
+++ Assets/Internal/Scripts/Survival/Game/Location/LocationEntity.cs
@@ -12,7 +12,7 @@ namespace Karabaev.Survival.Game.Location
   [UsedImplicitly]
   public class LocationEntity : Entity<LocationEntity.Context, LocationModel, LocationView>
   {
-    private readonly Dictionary<ObstacleModel, (ObstacleEntity, ReactiveProperty<int>.ValueChangedHandler)> _obstacleEntities = new();
+    private readonly Dictionary<ObstacleModel, (ObstacleEntity, ReactiveTrigger.TriggeredHandler)> _obstacleEntities = new();
 
     protected override UniTask OnCreatedAsync(Context context)
     {
@@ -30,33 +30,26 @@ namespace Karabaev.Survival.Game.Location
       Model.Obstacles.ItemRemoved -= Model_OnObstacleRemoved;
     }
 
-    private async void Model_OnObstacleRemoved(ObstacleModel oldItem, int index)
+    private void Model_OnObstacleRemoved(ObstacleModel oldItem, int index)
     {
-      View.RecalculateNavMesh();
       _obstacleEntities.Remove(oldItem, out var entity);
-      oldItem.CurrentHp.Changed -= entity.Item2;
-      await oldItem.DestroyFired.Set();
+      oldItem.DestroyFired.Triggered -= entity.Item2;
+      View.RecalculateNavMesh();
       DisposeChild(entity.Item1);
     }
 
-    private void Model_OnObstacleCurrentHpChanged(ObstacleModel model, int newValue)
-    {
-      if(newValue > 0)
-        return;
-
-      Model.Obstacles.Remove(model);
-    }
+    private void Model_OnObstacleCurrentHpChanged(ObstacleModel model) => Model.Obstacles.Remove(model);
 
     private async UniTask CreateObstacleAsync(ObstacleView view)
     {
       var model = new ObstacleModel(view.Descriptor);
       var entity = await CreateChildAsync<ObstacleEntity, ObstacleEntity.Context>(new ObstacleEntity.Context(model, view));
 
-      _obstacleEntities.Add(model, (entity, HpChangedHandler));
-      model.CurrentHp.Changed += HpChangedHandler;
+      _obstacleEntities.Add(model, (entity, ObstacleDestroyHandler));
+      model.DestroyFired.Triggered += ObstacleDestroyHandler;
       Model.Obstacles.Add(model);
 
-      void HpChangedHandler(int _, int newValue) => Model_OnObstacleCurrentHpChanged(model, newValue);
+      void ObstacleDestroyHandler() => Model_OnObstacleCurrentHpChanged(model);
     }
 
     protected override LocationModel CreateModel(Context context) => context.Model;
